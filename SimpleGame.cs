861188adb897a33a692818797bde3b9faa3a1aa0
ehdkq <ehdkq@umkc.edu>
2025-10-29 11:54:12 -0500

    more refactoring, implementing simple game logic for wins

---
+++ SimpleGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint_3
+{
+    internal class SimpleGame : BaseGame
+    {
+        public SimpleGame(int boardSize) : base(boardSize) { }
+
+        public override void MakeMove(int row, int col, Cell move)
+        {
+            if (State != GameState.InProgress || GameBoard[row, col] != Cell.Empty)
+            {
+                return;
+            }
+
+            GameBoard[row, col] = move;
+            MovesLeft--;
+            CheckForWin(row, col);
+
+            if (State == GameState.InProgress)
+            {
+                CurrentTurn = (CurrentTurn == Player.Blue) ? Player.Red : Player.Blue;
+            }
+        }
+
+        protected override void CheckForWin(int r, int c)
+        {
+            
+        }
+    }
+}
